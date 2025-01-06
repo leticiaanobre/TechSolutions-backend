4a4078145b33a2990bc00932f3f77e8b5ea7812f
Leticia <leticiavidal2910@gmail.com>
2025-01-06 11:10:10 -0300

    fix: added comparePassword function to userModel

--- src/models/User.js
+++ src/models/User.js
@@ -41,13 +41,19 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-userSchema.pre('save', async function (next) {
+// Hash da senha antes de salvar
+userSchema.pre('save', async function(next) {
   if (!this.isModified('password')) {
-    next();
+    return next();
   }
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
   next();
 });
+
+// Método para comparar senhas
+userSchema.methods.comparePassword = async function(candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
 
 export default mongoose.model('User', userSchema);
