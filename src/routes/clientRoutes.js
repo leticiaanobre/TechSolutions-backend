421f4761afadc36cca4716816d712310f68f6ace
Leticia <leticiavidal2910@gmail.com>
2025-01-08 16:26:32 -0300

    feat: added getUsers route to client

--- src/routes/clientRoutes.js
+++ src/routes/clientRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { createTask, getTasks, getHourBank, getTaskHistory } from '../controllers/clientController.js';
+import { createTask, getTasks, getHourBank, getTaskHistory, getUsers } from '../controllers/clientController.js';
 import auth from '../middlewares/auth.js';
 
 const router = express.Router();
 
 router.post('/tasks', auth, createTask);
+router.get('/users', auth, getUsers);
 router.get('/tasks', auth, getTasks);
 router.get('/tasks/history', auth, getTaskHistory);
 router.get('/hour-bank', auth, getHourBank);
